2e2cc20b0423fa7225a15bc64aeb55317b317de9
MihaiCHG <52129403+MihaiCHG@users.noreply.github.com>
2020-05-06 22:27:15 +0300

    V0.2
    Adaugate o parte din produse
    Adaugate o parte din ingrediente
    Momentan fara dll.
    (Mihai Heghea)

---
+++ Produs/Produs/Ingredient.cs
@@ -0,0 +1,656 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Restaurant
+{
+    public interface Ingredient
+    {
+        string Show();
+    }
+
+    public class SosAlb : Ingredient
+    {
+        private string _nume;
+
+        public SosAlb()
+        {
+            _nume = "Sos alb";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class Bacon : Ingredient
+    {
+        private string _nume;
+
+        public Bacon()
+        {
+            _nume = "Bacon";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class Ou : Ingredient
+    {
+        private string _nume;
+
+        public Ou()
+        {
+            _nume = "Ou";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class Parmezan : Ingredient
+    {
+        private string _nume;
+
+        public Parmezan()
+        {
+            _nume = "Parmezan";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class CarneDeVita : Ingredient
+    {
+        private string _nume;
+
+        public CarneDeVita()
+        {
+            _nume = "Carne De Vita";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class Ardei : Ingredient
+    {
+        private string _nume;
+
+        public Ardei()
+        {
+            _nume = "Ardei";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class Rosie : Ingredient
+    {
+        private string _nume;
+
+        public Rosie()
+        {
+            _nume = "Rosie";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class FructeDeMare : Ingredient
+    {
+        private string _nume;
+        private string _detalii;
+
+        public FructeDeMare()
+        {
+            _nume = "Fructe De Mare";
+            _detalii = "scoici, fasolare, trufe de mare";
+        }
+        public string Show()
+        {
+            return _nume+": "+_detalii;
+
+        }
+    }
+
+    public class Busuioc : Ingredient
+    {
+        private string _nume;
+
+        public Busuioc()
+        {
+            _nume = "Busuioc";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class Gongorzola : Ingredient
+    {
+        private string _nume;
+
+        public Gongorzola()
+        {
+            _nume = "Gongorzola";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class Cascaval : Ingredient
+    {
+        private string _nume;
+
+        public Cascaval()
+        {
+            _nume = "Cascaval";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class Mozzarela : Ingredient
+    {
+        private string _nume;
+
+        public Mozzarela()
+        {
+            _nume = "Mozzarela";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class PieptDePui : Ingredient
+    {
+        private string _nume;
+
+        public PieptDePui()
+        {
+            _nume = "Piept De Pui";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class Crutoane : Ingredient
+    {
+        private string _nume;
+
+        public Crutoane()
+        {
+            _nume = "Crutoane";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class Dresing : Ingredient
+    {
+        private string _nume;
+        private string _detalii;
+
+        public Dresing()
+        {
+            _nume = "Dresing de iaurt";
+            _detalii = "zeama unei lamai, 1 lingurita zahar, 2 catei de usturoi maruntit, 125 g iaurt grecesc, \n";
+            _detalii += "1 lingura ulei de masline, 2 linguri patrunjel maruntit, sare, piper";
+        }
+        public string Show()
+        {
+            return _nume+": "+_detalii;
+        }
+    }
+
+    public class Cartof : Ingredient
+    {
+        private string _nume;
+
+        public Cartof()
+        {
+            _nume = "Cartof";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class Castraveti : Ingredient
+    {
+        private string _nume;
+
+        public Castraveti()
+        {
+            _nume = "Castraveti";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class ProsciutoCrudo : Ingredient
+    {
+        private string _nume;
+
+        public ProsciutoCrudo()
+        {
+            _nume = "Prosciuto Crudo";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class Oregano : Ingredient
+    {
+        private string _nume;
+
+        public Oregano()
+        {
+            _nume = "Oregano";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class UleiDeMasline : Ingredient
+    {
+        private string _nume;
+
+        public UleiDeMasline()
+        {
+            _nume = "Ulei De Masline";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class SuncaPresta : Ingredient
+    {
+        private string _nume;
+
+        public SuncaPresta()
+        {
+            _nume = "Sunca Presta";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class Branza : Ingredient
+    {
+        private string _nume;
+
+        public Branza()
+        {
+            _nume = "Branza";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class SalataVerde : Ingredient
+    {
+        private string _nume;
+
+        public SalataVerde()
+        {
+            _nume = "Salata Verde";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class Smantana : Ingredient
+    {
+        private string _nume;
+        private string _detalii;
+
+        public Smantana()
+        {
+            _nume = "Salata Verde";
+            _detalii = "3% grasime";
+        }
+        public string Show()
+        {
+            return _nume+": "+_detalii;
+        }
+    }
+    public class Morcov : Ingredient
+    {
+        private string _nume;
+
+        public Morcov()
+        {
+            _nume = "Morcov";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class BurtaVita : Ingredient
+    {
+        private string _nume;
+
+        public BurtaVita()
+        {
+            _nume = "Burta Vita";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class Otet : Ingredient
+    {
+        private string _nume;
+
+        public Otet()
+        {
+            _nume = "Otet";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class Usturoi : Ingredient
+    {
+        private string _nume;
+
+        public Usturoi()
+        {
+            _nume = "Usturoi";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class Bors : Ingredient
+    {
+        private string _nume;
+
+        public Bors()
+        {
+            _nume = "Bors";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+
+    }
+    public class CarnePui : Ingredient
+    {
+        private string _nume;
+
+        public CarnePui()
+        {
+            _nume = "Carne Pui";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class Gris : Ingredient
+    {
+        private string _nume;
+
+        public Gris()
+        {
+            _nume = "Gris";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class Telina : Ingredient
+    {
+        private string _nume;
+
+        public Telina()
+        {
+            _nume = "Telina";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class Ceapa : Ingredient
+    {
+        private string _nume;
+
+        public Ceapa()
+        {
+            _nume = "Ceapa";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class Fasole : Ingredient
+    {
+        private string _nume;
+
+        public Fasole()
+        {
+            _nume = "Fasole";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class Patrunej : Ingredient
+    {
+        private string _nume;
+
+        public Patrunej()
+        {
+            _nume = "Patrunej";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+    public class Masline : Ingredient
+    {
+        private string _nume;
+
+        public Masline()
+        {
+            _nume = "Masline";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+    public class Cimbru : Ingredient
+    {
+        private string _nume;
+
+        public Cimbru()
+        {
+            _nume = "Cimbru";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+    public class Sare : Ingredient
+    {
+        private string _nume;
+
+        public Sare()
+        {
+            _nume = "Sare";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+    public class Piper : Ingredient
+    {
+        private string _nume;
+
+        public Piper()
+        {
+            _nume = "Piper";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+    public class FicateiPui : Ingredient
+    {
+        private string _nume;
+
+        public FicateiPui()
+        {
+            _nume = "Ficatei de Pui";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class Vin : Ingredient
+    {
+        private string _nume;
+
+        public Vin()
+        {
+            _nume = "Vin rosu";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+    public class AripioarePui : Ingredient
+    {
+        private string _nume;
+
+        public AripioarePui()
+        {
+            _nume = "Aripioare De Pui";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+    public class Curry : Ingredient
+    {
+        private string _nume;
+
+        public Curry()
+        {
+            _nume = "Curry";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+    public class ArdeiIuti : Ingredient
+    {
+        private string _nume;
+
+        public ArdeiIuti()
+        {
+            _nume = "Ardei Iuti";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class Faina : Ingredient
+    {
+        private string _nume;
+
+        public Faina()
+        {
+            _nume = "Faina";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+    public class Mustar : Ingredient
+    {
+        private string _nume;
+
+        public Mustar()
+        {
+            _nume = "Mustar";
+        }
+        public string Show()
+        {
+            return _nume;
+        }
+    }
+
+}
---
+++ Produs/Produs/Produs.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Security;
+
+namespace Restaurant
+{
+    public abstract class Produs
+    {
+        protected string _nume;
+        protected List<Ingredient> _ingrediente;
+        protected double _gramaj;
+        protected double _pret;
+
+
+        public string descriere()
+        {
+            string desc = _nume + "\t";
+            for(int i = 0; i < _ingrediente.Count; i++)
+            {
+                desc += _ingrediente[i] + "\n";
+            }
+            desc += "Gramaj: " + _gramaj + " Pret: " + _pret;
+            return desc;
+        }
+
+    }
+
+
+    public class SpaghettiCarbonara : Produs
+    {
+        public SpaghettiCarbonara()
+        {
+            _nume = "Spaghetti Carbonara";
+            _gramaj = 350.0;
+            _pret = 28.0;
+            _ingrediente.Add(new SosAlb());
+            _ingrediente.Add(new Bacon());
+            _ingrediente.Add(new Ou());
+            _ingrediente.Add(new Parmezan());
+        }
+    }
+
+    public class SpaghettiBolognese : Produs
+    {
+        public SpaghettiBolognese()
+        {
+            _nume = "Spaghetti Bolognese";
+            _gramaj = 350.0;
+            _pret = 28.0;
+            _ingrediente.Add(new CarneDeVita());
+            _ingrediente.Add(new Ardei());
+            _ingrediente.Add(new Rosie());
+            _ingrediente.Add(new Parmezan());
+        }
+    }
+
+    public class LinguiniConFrttiDiMare : Produs
+    {
+        public LinguiniConFrttiDiMare()
+        {
+            _nume = "Linguini Con Frtti Di Mare";
+            _gramaj = 350.0;
+            _pret = 41.0;
+            _ingrediente.Add(new FructeDeMare());
+            _ingrediente.Add(new Rosie());
+            _ingrediente.Add(new Busuioc());
+        }
+    }
+
+    public class PenneQuattroFormaggi : Produs
+    {
+        public PenneQuattroFormaggi()
+        {
+            _nume = "Penne Quattro Formaggi";
+            _gramaj = 350.0;
+            _pret = 30.0;
+            _ingrediente.Add(new SosAlb());
+            _ingrediente.Add(new Gongorzola());
+            _ingrediente.Add(new Cascaval());
+            _ingrediente.Add(new Parmezan());
+            _ingrediente.Add(new Mozzarela());
+        }
+    }
+
+    public class Lasagna : Produs
+    {
+        public Lasagna()
+        {
+            _nume = "Lasagna";
+            _gramaj = 350.0;
+            _pret = 29.0;
+            _ingrediente.Add(new CarneDeVita());
+            _ingrediente.Add(new Mozzarela());
+        }
+    }
+//Gustari Reci
+    public class SalataCaesar : Produs
+    {
+        public SalataCaesar()
+        {
+            _nume = "Salata Caesar";
+            _gramaj = 250.0;
+            _pret = 26.0;
+            _ingrediente.Add(new Rosie());
+            _ingrediente.Add(new Ardei());
+            _ingrediente.Add(new PieptDePui());
+            _ingrediente.Add(new Crutoane());
+            _ingrediente.Add(new Dresing());
+        }
+    }
+
+    public class SalataGreceasca : Produs
+    {
+        public SalataGreceasca()
+        {
+            _nume = "Salata Caesar";
+            _gramaj = 250.0;
+            _pret = 25.0;
+            _ingrediente.Add(new Castraveti());
+            _ingrediente.Add(new Rosie());
+            _ingrediente.Add(new Ardei());
+            _ingrediente.Add(new Cartof());
+            _ingrediente.Add(new Mozzarela());
+            _ingrediente.Add(new Dresing());
+        }
+    }
+
+    public class SalataCapresse : Produs
+    {
+        public SalataCapresse()
+        {
+            _nume = "Salata Capresse";
+            _gramaj = 250.0;
+            _pret = 30.0;
+            _ingrediente.Add(new Mozzarela());
+            _ingrediente.Add(new Rosie());
+            _ingrediente.Add(new ProsciutoCrudo());
+            _ingrediente.Add(new Oregano());
+            _ingrediente.Add(new UleiDeMasline());
+        }
+    }
+
+    public class SalataBulgareasca : Produs
+    {
+        public SalataBulgareasca()
+        {
+            _nume = "Salata Bulgareasca";
+            _gramaj = 250.0;
+            _pret = 16.0;
+            _ingrediente.Add(new Rosie());
+            _ingrediente.Add(new Castraveti());
+            _ingrediente.Add(new SuncaPresta());
+            _ingrediente.Add(new Branza());
+            _ingrediente.Add(new Masline());
+            _ingrediente.Add(new SalataVerde());
+        }
+    }
+//Supe/Ciorbe
+    public class CiorbaRadauteana : Produs
+    {
+        public CiorbaRadauteana()
+        {
+            _nume = "Ciorba Radauteana";
+            _gramaj = 300.0;
+            _pret = 13.0;
+            _ingrediente.Add(new Ou());
+            _ingrediente.Add(new Smantana());
+            _ingrediente.Add(new PieptDePui());
+            _ingrediente.Add(new Morcov());
+        }
+    }
+
+    public class CiorbaBurta : Produs
+    {
+        public CiorbaBurta()
+        {
+            _nume = "Ciorba Burta";
+            _gramaj = 300.0;
+            _pret = 14.0;
+            _ingrediente.Add(new Ou());
+            _ingrediente.Add(new Smantana());
+            _ingrediente.Add(new BurtaVita());
+            _ingrediente.Add(new Otet());
+            _ingrediente.Add(new Usturoi());
+        }
+    }
+    public class CiorbaPerisoare : Produs
+    {
+        public CiorbaPerisoare()
+        {
+            _nume = "Ciorba Perisoare";
+            _gramaj = 300.0;
+            _pret = 11.0;
+            _ingrediente.Add(new Ou());
+            _ingrediente.Add(new Bors());
+            _ingrediente.Add(new CarnePui());
+            _ingrediente.Add(new Gris());
+            _ingrediente.Add(new Telina());
+            _ingrediente.Add(new Ceapa());
+            _ingrediente.Add(new Morcov());
+        }
+    }
+
+    public class CiorbaDeVacuta : Produs
+    {
+        public CiorbaDeVacuta()
+        {
+            _nume = "Ciorba De Vacuta";
+            _gramaj = 300.0;
+            _pret = 11.0;
+            _ingrediente.Add(new CarneDeVita());
+            _ingrediente.Add(new Bors());
+            _ingrediente.Add(new Telina());
+            _ingrediente.Add(new Morcov());
+            _ingrediente.Add(new Ceapa());
+            _ingrediente.Add(new Ardei());
+        }
+    }
+
+    public class CiorbaDeFasole : Produs
+    {
+        public CiorbaDeFasole()
+        {
+            _nume = "Ciorba De Fasole";
+            _gramaj = 300.0;
+            _pret = 7.0;
+            _ingrediente.Add(new Fasole());
+            _ingrediente.Add(new Bors());
+            _ingrediente.Add(new Telina());
+            _ingrediente.Add(new Morcov());
+            _ingrediente.Add(new Patrunej());
+            _ingrediente.Add(new Ceapa());
+        }
+    }
+    //Preparate pui
+
+    public class PietDePuiLaGratar : Produs
+    {
+        public PietDePuiLaGratar()
+        {
+            _nume = "Piet De Pui La Gratar";
+            _gramaj = 200.0;
+            _pret = 13.0;
+            _ingrediente.Add(new PieptDePui());
+            _ingrediente.Add(new Usturoi());
+            _ingrediente.Add(new UleiDeMasline());
+            _ingrediente.Add(new Cimbru());
+            _ingrediente.Add(new Sare());
+            _ingrediente.Add(new Piper());
+        }
+    }
+
+    public class PietDePuiCuSosGongorzola : Produs
+    {
+        public PietDePuiCuSosGongorzola()
+        {
+            _nume = "Piet De Pui Cu Sos Gongorzola";
+            _gramaj = 250.0;
+            _pret = 21.0;
+            _ingrediente.Add(new PieptDePui());
+            _ingrediente.Add(new Smantana());
+            _ingrediente.Add(new Gongorzola());
+            _ingrediente.Add(new Parmezan());
+            _ingrediente.Add(new Usturoi());
+            _ingrediente.Add(new Sare());
+            _ingrediente.Add(new Piper());
+        }
+    }
+
+    public class FicateiPasare : Produs
+    {
+        public FicateiPasare()
+        {
+            _nume = "Ficatei Pasare cu ceapa si sos de rosii";
+            _gramaj = 200.0;
+            _pret = 30.0;
+            _ingrediente.Add(new FicateiPui());
+            _ingrediente.Add(new Ceapa());
+            _ingrediente.Add(new Morcov());
+            _ingrediente.Add(new Rosie());
+            _ingrediente.Add(new Usturoi());
+            _ingrediente.Add(new Vin());
+            _ingrediente.Add(new UleiDeMasline());
+            _ingrediente.Add(new Sare());
+            _ingrediente.Add(new Piper());
+        }
+    }
+
+    public class AripioarePicante : Produs
+    {
+        public AripioarePicante()
+        {
+            _nume = "Aripioare Picante";
+            _gramaj = 100.0;
+            _pret = 6.0;
+            _ingrediente.Add(new AripioarePui());
+            _ingrediente.Add(new Cimbru());
+            _ingrediente.Add(new Curry());
+            _ingrediente.Add(new Oregano());
+            _ingrediente.Add(new ArdeiIuti());
+            _ingrediente.Add(new Sare());
+            _ingrediente.Add(new Piper());
+        }
+    }
+
+    public class CiulamaDePuiCuBranzaSiMamaliga : Produs
+    {
+        public CiulamaDePuiCuBranzaSiMamaliga()
+        {
+            _nume = "Ciulama De Pui Cu Branza Si Mamaliga";
+            _gramaj = 500.0;
+            _pret = 18.0;
+            _ingrediente.Add(new PieptDePui());
+            _ingrediente.Add(new Ceapa());
+            _ingrediente.Add(new Usturoi());
+            _ingrediente.Add(new Morcov());
+            _ingrediente.Add(new Smantana());
+            _ingrediente.Add(new Faina());
+            _ingrediente.Add(new Sare());
+            _ingrediente.Add(new Piper());
+        }
+    }
+
+    public class PuiShangai : Produs
+    {
+        public PuiShangai()
+        {
+            _nume = "Pui Shangai";
+            _gramaj = 250.0;
+            _pret = 20.0;
+            _ingrediente.Add(new PieptDePui());
+            _ingrediente.Add(new Ou());
+            _ingrediente.Add(new Mustar());
+            _ingrediente.Add(new Sare());
+        }
+    }
+}
