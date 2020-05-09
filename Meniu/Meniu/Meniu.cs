e952bf83feaeab37dc7f2e8036d6ac76627ecdd3
MihaiCHG <52129403+MihaiCHG@users.noreply.github.com>
2020-05-09 15:35:06 +0300

    V0.3
    Adaugat clasa meniu si clasa TipProdus

---
+++ Meniu/Meniu/Meniu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Restaurant
+{
+    public class Meniu
+    {
+        private static Meniu _instance=null;
+        private List<TipProdus> _productList;
+
+
+        private Meniu()
+        {
+
+        }
+
+        public static Meniu GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Meniu();
+            }
+            return _instance;
+        }
+    }
+}
