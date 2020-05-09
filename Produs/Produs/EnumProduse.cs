b4affc8d6e55c549d69a8ceebfb5325c0d43de96
MihaiCHG <52129403+MihaiCHG@users.noreply.github.com>
2020-05-09 15:50:42 +0300

    V0.4
    Adaugat enum pentru fiecare produs, pentru a fi folosit la fabrica.

---
+++ Produs/Produs/EnumProduse.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Produs
+{
+    enum ProductName
+    {
+        //Paste
+        PSpaghettiCarbonara,
+        PSpaghettiBolognese,
+        PLinguiniConFrttiDiMare,
+        PPennQuattroFormaggi,
+        PLasagna,
+        //Gustari reci
+        PSalataCaesar,
+        PSalataGreceasca,
+        PSalatCapresse,
+        PSalataBulgareasca,
+        //Supe/Ciorbe
+        PCiorbaRadauteana,
+        PCiorbaBurta,
+        PCiorbaPerisoare,
+        PCiorbaDeVacuta,
+        PCiorbaDeFasole,
+        //Preparate Pui
+        PPietDePuiLaGratar,
+        PPieptDePuiCuSosGrogonzola,
+        PFicateiPasare,
+        PAripioarePicante,
+        PCiulamaDePuiCuBranzaSiMamaliguta,
+        PPuiShanghai,
+        PPiFteluteMoldovenesti,
+        PCordonBleu,
+        //Preparate Porc
+        PCeafaLaGratar,
+        PCotletLaGratar,
+        PFrigaruiDePorcCuLegume,
+        PSnitelPorc,
+        PCiolanDePorcCuVarzaCalita,
+        PTochituraMoldoveneasca,
+        PMici,
+        PFasoleCuCiolan,
+        //Deserturi
+        PClatiteCuGem,
+        PClatiteCuFinetti,
+        PClatiteCuInghetataTopingSiFrisca,
+        PPapanasi,
+        PInghetataAsortata,
+        PTiramisu,
+        PBaclava,
+        PCheeseCake,
+        PTortCuFructe,
+        PTortCuCiocolata
+    }
+}
